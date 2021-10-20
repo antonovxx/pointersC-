2f84db9e15bc7fc5f68d8526407e85762658c709
antonkurin <antonkurin1@gmail.com>
2021-10-20 17:15:12 +0300

    Initial Commit

---
+++ pointersC++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pointersC++
+//
+//  Created by Антон Курин on 20.10.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
